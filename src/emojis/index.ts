699b5332ef03d8bbda850e370ed2b8140d601513
doritosxxx <georgyvav@ya.ru>
2020-12-20 08:01:17 +0300

    Emoji constants

---
+++ src/emojis/index.ts
@@ -0,0 +1,15 @@
+class Emojis {
+	private constructor(){}
+
+	static A = '🇦'
+	static B = '🇧'
+	static C = '🇨'
+	static D = '🇩'
+	static E = '🇪'
+	static I = '🇮'
+	static P = '🇵'
+	static Z = '🇿'
+}
+
+
+export default Emojis
